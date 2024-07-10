37fe7d637df2c739ad631577b0c48f18622072ff
André Lucas Junqueira <130708772+AndreLJunqueira@users.noreply.github.com>
2024-07-10 19:36:20 -0300

    aprimorando

--- Fokus-projeto-base/script-crud.js
+++ Fokus-projeto-base/script-crud.js
@@ -1,6 +1,18 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const forAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
 
 btnAdicionarTarefa.addEventListener('click', () => {
     forAdicionarTarefa.classList.toggle('hidden')
+})
+
+forAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
